10a7cb7470dd76b01e7a42937f09f282476e18d4
DarkGuibrine <darkgui@protonmail.com>
2026-05-24 01:54:03 -0300

    funciona

--- flake.nix
+++ flake.nix
@@ -15,14 +15,19 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
         Medieval = "import"; # Apenas para fins de contexto
       in
       {
         packages = {
           shiru = pkgs.callPackage ./pkgs/shiru/default.nix { };
           hayase = pkgs.callPackage ./pkgs/hayase/default.nix { };
-          hellium = pkgs.callPackeg ./ppkgs/hellium/defalt.nix { };
+          hellium = pkgs.callPackage ./pkgs/hellium/default.nix { };
 
           # Você também pode definir um pacote padrão (quando rodar 'nix build' sem especificar o nome)
           # default = self.packages.${system}.pacote-a;
